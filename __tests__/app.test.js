5535b4a46f5e39c4562b862f6b20fac9e69f63f3
joshua360x <jkw5499@psu.edu>
2022-03-15 20:24:53 -0400

    added get response by id request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,7 @@ const app = require('../lib/app');
 const Phone = require('../lib/models/Phone');
 
 describe('any-api routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
   });
 
@@ -28,16 +28,24 @@ describe('any-api routes', () => {
 
 
   it('get all of our phones', async () => {
-    const expected = await Phone.getAllPhones();
-    // const expected = {
-    //   name: 'blubberPhone',
-    //   color: 'space-blue',
-    //   yearReleased: 2008,
-    //   inventor: 'guy WIth MasK',
-    // };
+    // const expected = await Phone.getAllPhones();
+    const expected = [{
+      name: 'blubberPhone',
+      id: expect.any(String),
+      color: 'space-blue',
+      yearReleased: 2008,
+      inventor: 'guy WIth MasK',
+    }];
     const res = await request(app).get('/api/v1/phones');
 
     expect(res.body).toEqual(expected);
+  });
+
+
+  it('get a phone by its ID', async () => {
+    const expected = await Phone.getByASpecificID(1);
+    const res = await request(app).get(`/api/v1/phones/${expected.id}`);
+    expect(res.body).toEqual({ ...expected });
   });
 
 });
